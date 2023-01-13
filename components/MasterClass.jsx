769946d63b6a2f50798bca688bcfcfcd77a24a27
hazratamin98 <hazratamin98@gmail.com>
2023-01-13 14:17:18 +0500

    Page setup

---
+++ components/MasterClass.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MasterClass = () => {
+  return (
+    <div>MasterClass</div>
+  )
+}
+
+export default MasterClass
---
+++ components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Live from "./Live";
+import MasterClass from "./MasterClass";
+import Messages from "./Messages";
+import Estimate from "./Estimate";
+import Price from "./Price";
+import Start from "./Start";
+import Footer from "./Footer";
+
+export {
+    Navbar , Hero , Live , MasterClass , Messages , Estimate , Price , Start , Footer
+}
