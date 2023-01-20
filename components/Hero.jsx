b1f67d37549b3562a775dc533d8afd7ee1701c00
hazratamin98 <hazratamin98@gmail.com>
2023-01-20 14:22:46 +0500

    after classes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,8 +6,12 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-center'>
     <div className='  flex-col text-center space-y-6 '>
-      <h1 className='font-bold text-[55px] leading-[66px] center mt-40 text-[#FFFFFF]' >
-       Your own video platform to <br/> monetize your expertise
+      <h1 className='font-bold text-[55px] leading-[66px] center mt-40  text-[#FFFFFF]' >
+       Your own video platform to <br/> 
+       <span  className=" relative before:block before:absolute before:inset-5 before:mt-6 before:py-4   before:bg-[#5A27E7]">
+    <span className="relative text-white">  monetize your expertise</span>
+  </span>
+     
       </h1>
       <p className='font-medium text-lg text-[#999CA0] leading-[27px] '  >Lynq provides an all-in-one white label solution to manage and offer paid <br/> video-based one-on-ones and masterclasses to your followers.</p>
     <Button label="Get Start for Free"/>
@@ -20,7 +24,11 @@ const Hero = () => {
 
       <div className='  flex-col text-center space-y-6 '>
       <h1 className='font-bold text-[38px] leading-[66px]  text-[#FFFFFF]' >
-     Selling expertise and interaction with your <br/> audience is your next big source of revenue
+     Selling expertise and interaction with your <br/> audience is
+      <span  className=" relative before:block before:absolute before:inset-5 before:mt-4 before:py-4    before:bg-[#5A27E7]">
+    <span className="relative text-white">  your next big source of revenue</span>
+  </span>
+     
       </h1>
       <p className='font-medium text-lg text-[#999CA0] leading-[27px] '>Let Lynq helps you launch your own expertise video channel</p>
     <Button label="Launch Your Expertise Channel For Free"/>
--- components/Start.jsx
+++ components/Start.jsx
@@ -4,7 +4,11 @@ import {Button} from "../components"
 const Start = () => {
   return (
     <div className='bg-[#1A1B1C] md:rounded-[30px] mx-0 md:mx-[210px] mb-[50px] text-center py-9 space-y-[40px]   '>
-      <h1 className='text-[#FBFF12] text-[40px] font-bold leading-[67px] px-28 text-center min-w-[302px] '>Start monetizing your expertise</h1>
+      <h1 className='text-[#FBFF12] text-[40px] font-bold leading-[67px] px-28 text-center min-w-[302px] '>
+          <span  className=" relative before:block before:absolute before:inset-5 before:mt-4 before:py-4    before:bg-[#5A27E7]">
+    <span className="relative ">  Start monetizing your expertise</span>
+  </span>
+       </h1>
 
       <Button label="Get Strat For Free" />
 
