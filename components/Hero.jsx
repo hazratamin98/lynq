624ff105d94f07ec167179534728f7974f7a56b3
hazratamin98 <hazratamin98@gmail.com>
2023-01-20 14:59:26 +0500

    text size on different screen

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-center'>
     <div className='  flex-col text-center space-y-6 '>
-      <h1 className='font-bold text-[55px] leading-[66px] center mt-40  text-[#FFFFFF]' >
+      <h1 className='font-bold md:text-[55px] text-[38px] md:leading-[66px] leading-[38px] center mt-40 inline-block text-[#FFFFFF]' >
        Your own video platform to <br/> 
-       <span  className=" relative before:block before:absolute before:inset-5 before:mt-6 before:py-4   before:bg-[#5A27E7]">
+       <span  className=" relative before:block before:absolute before:inset-5 md:before:mt-6 before:py-4   before:bg-[#5A27E7]">
     <span className="relative text-white">  monetize your expertise</span>
   </span>
      
       </h1>
-      <p className='font-medium text-lg text-[#999CA0] leading-[27px] '  >Lynq provides an all-in-one white label solution to manage and offer paid <br/> video-based one-on-ones and masterclasses to your followers.</p>
+      <p className='md:font-medium font-normal  md:text-lg text-base text-[#999CA0] md:leading-[27px] leading-[22px]'  >Lynq provides an all-in-one white label solution to manage and offer paid <br/> video-based one-on-ones and masterclasses to your followers.</p>
     <Button label="Get Start for Free"/>
     </div>
  
@@ -23,14 +23,14 @@ const Hero = () => {
    
 
       <div className='  flex-col text-center space-y-6 '>
-      <h1 className='font-bold text-[38px] leading-[66px]  text-[#FFFFFF]' >
+      <h1 className='font-bold md:text-[55px] text-[38px] md:leading-[66px] leading-[38px]  text-[#FFFFFF]' >
      Selling expertise and interaction with your <br/> audience is
       <span  className=" relative before:block before:absolute before:inset-5 before:mt-4 before:py-4    before:bg-[#5A27E7]">
     <span className="relative text-white">  your next big source of revenue</span>
   </span>
      
       </h1>
-      <p className='font-medium text-lg text-[#999CA0] leading-[27px] '>Let Lynq helps you launch your own expertise video channel</p>
+      <p className='md:font-medium font-normal  md:text-lg text-base text-[#999CA0] leading-[27px] '>Let Lynq helps you launch your own expertise video channel</p>
     <Button label="Launch Your Expertise Channel For Free"/>
     </div>
     </div>
--- components/Start.jsx
+++ components/Start.jsx
@@ -4,7 +4,7 @@ import {Button} from "../components"
 const Start = () => {
   return (
     <div className='bg-[#1A1B1C] md:rounded-[30px] mx-0 md:mx-[210px] mb-[50px] text-center py-9 space-y-[40px]   '>
-      <h1 className='text-[#FBFF12] text-[40px] font-bold leading-[67px] px-28 text-center min-w-[302px] '>
+      <h1 className='text-[#FBFF12]  md:text-[34px] text-[30px] font-bold leading-[67px] px-28 text-center min-w-[302px] '>
           <span  className=" relative before:block before:absolute before:inset-5 before:mt-4 before:py-4    before:bg-[#5A27E7]">
     <span className="relative ">  Start monetizing your expertise</span>
   </span>
