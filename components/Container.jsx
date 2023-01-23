5d3f9cebe515f194a64405aedbd72ca651443535
hazratamin98 <hazratamin98@gmail.com>
2023-01-23 14:59:07 +0500

    complete setup

--- components/Container.jsx
+++ components/Container.jsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react'
 
 const Container = ({Children}) => {
   return (
-    <div className="max-w-[1080px] mx-auto   py-5 px-28">{Children}</div>
+    <div className="max-w-[1080px] mx-auto   py-5 px-28 bg-black text-white">{Children}</div>
   )
 }
 
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
     <div className='  flex-col text-center space-y-6 '>
       <h1 className='font-bold md:text-[55px] text-[38px] md:leading-[66px] leading-[38px] center mt-40 inline-block text-[#FFFFFF]' >
        Your own video platform to <br/> 
-       <span  className=" relative before:block before:absolute before:inset-5 md:before:mt-6 before:py-4 md:visible hidden   before:bg-[#5A27E7]">
+       {/* <span  className=" relative before:block before:absolute before:inset-5 md:before:mt-6 before:py-4 md:visible    before:bg-[#5A27E7]"> */}
     <span className="relative text-white">  monetize your expertise</span>
-  </span>
+  {/* </span> */}
      
       </h1>
       <p className='md:font-medium font-normal  md:text-lg text-base text-[#999CA0] md:leading-[27px] leading-[22px]'  >Lynq provides an all-in-one white label solution to manage and offer paid <br/> video-based one-on-ones and masterclasses to your followers.</p>
